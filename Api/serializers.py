dbe5f0a0165efb7b288e9733e46e8b78b00e52b9
naveen <naveentiwari112@gmail.com>
2021-02-24 19:50:49 +0530

    first commit

---
+++ Api/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from .models import Category,Book,Product
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Category
+        fields=('id','title')
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:   
+        model = Book
+        fields=(
+            'id',
+            'title',
+            'category',
+            'isbn',
+            'pages',
+            'price',
+            'stock',
+            'description',
+            'imageurl',
+            'status',
+            'date_created' 
+        )
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields=(
+            'id',
+            'product_tag',
+            'name',
+            'category',
+            'price',
+            'stock',
+            'description',
+            'imageurl',
+            'status',
+            'date_created'             
+        )
